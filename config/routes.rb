a91b05e282a9af1739154aff55a802e661533153
natsumi22222222 <110472548+natsumi22222222@users.noreply.github.com>
2022-08-31 13:07:48 +0000

    [Update]4-25

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'index/show'
-  get 'index/edit'
+
   devise_for :users
   get "homes/about" => "homes#about", as: "about"
   root 'homes#top'
-  
+  resources :users,only:[:index,:show,:edit]
+  resources :books,only:[:index,:show,:edit]
+
 
 end
