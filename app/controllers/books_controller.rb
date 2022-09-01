2c76c2e20a9f015c8e8fb85e49e1649b208f011c
natsumi22222222 <110472548+natsumi22222222@users.noreply.github.com>
2022-09-01 08:22:36 +0000

    [4-25]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,17 @@
 class BooksController < ApplicationController
   def index
+    @book= Book.new
+    @Books= Book.all
+  end
+
+  def create
+    @book= Book.new(book_params)
+    @book.save
+    redirect_to book_path(@books.id)
   end
 
   def show
+    
   end
 
   def edit
