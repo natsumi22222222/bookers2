7b629ad0d6628dcf8c2e5d46acfe55a35baeefc3
natsumi22222222 <110472548+natsumi22222222@users.noreply.github.com>
2022-09-01 13:19:20 +0000

    [Update]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,14 +6,26 @@ class BooksController < ApplicationController
 
   def create
     @book= Book.new(book_params)
-    @book.save
-    redirect_to book_path(@books.id)
+    # @book.user_id=current_user.id
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @book= Book.all
+      render :index
+    end
   end
 
   def show
-    
+    @book= Book.find(params[:id])
   end
 
   def edit
+    @book= Book.find(params[:id])
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
